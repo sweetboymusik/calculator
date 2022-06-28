fc3346626fa8bb41b1c0aaa8c457e4e2de273827
Elliott Butt <ellbutt@gmail.com>
2022-06-28 16:19:08 -0230

    Created some operator functions

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const numbersArr = document.querySelectorAll(".number");
 const outputEl = document.querySelector(".output");
 const clearEl = document.querySelector(".clear");
 const decimalEl = document.querySelector(".decimal");
+const equalsEl = document.querySelector(".equals");
+const addEl = document.querySelector(".add");
+const subtractEl = document.querySelector(".subtract");
+const multiplyEl = document.querySelector(".multiply");
+const divideEl = document.querySelector(".divide");
+
+const outputCalculatedArray = [];
 
 numbersArr.forEach((numberEl) => {
   numberEl.addEventListener("click", (e) => {
@@ -16,3 +23,33 @@ clearEl.addEventListener("click", (e) => (outputEl.textContent = ""));
 decimalEl.addEventListener("click", (e) => {
   if (!outputEl.textContent.includes(".")) outputEl.textContent += ".";
 });
+
+// Operators
+
+function clearOutputField() {
+  outputEl.textContent = "";
+}
+
+function add(event) {
+  if (outputCalculatedArray.length === 0) {
+    outputCalculatedArray.push(Number(outputEl.textContent));
+    clearOutputField();
+  } else {
+    outputCalculatedArray.push(
+      outputCalculatedArray[outputCalculatedArray.length - 1] +
+        Number(outputEl.textContent)
+    );
+
+    clearOutputField();
+    console.log(outputCalculatedArray);
+  }
+}
+
+function equals(event) {
+  outputEl.textContent =
+    outputCalculatedArray[outputCalculatedArray.length - 1];
+}
+
+addEl.addEventListener("click", add);
+
+equalsEl.addEventListener("click", equals);
